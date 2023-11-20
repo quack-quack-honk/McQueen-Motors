229d7156b8377ebe8050ab1bcd597f98f9bf162b
Ben <doddsbt@gmail.com>
2023-11-20 04:24:55 +0000

    Update SensorTest.ino

--- Line Detection Code/SensorTest/SensorTest.ino
+++ Line Detection Code/SensorTest/SensorTest.ino
@@ -2,10 +2,10 @@ int readPin = A0;
 
 int val = 0; 
 void setup() {
-	Serial.begin(9600);           //  setup serial
+	Serial.begin(9600);           //setup serial at given baud rate
 }
 
 void loop() {
-	val = analogRead(readPin);  // read the input pin
-	Serial.println(val);          // debug value
+	val = analogRead(readPin);  //read the input pin
+	Serial.println(val);          //debug value over serial
 }
