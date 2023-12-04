f099b409eb89ecc7587662d0f9f87dd506c4eda5
Ben <doddsbt@gmail.com>
2023-12-04 19:32:14 +0000

    Setup Sensor test code for 3 sensors

--- Line Detection Code/SensorTest/SensorTest.ino
+++ Line Detection Code/SensorTest/SensorTest.ino
@@ -1,18 +1,23 @@
-int readPin = A0; 
 int threshhold = 900;
 int anVal = 0;
+int sens1 = 0;
+int sens2 = 0;
+int sens3 = 0;
 
 void setup() {
 	Serial.begin(9600);   //setup serial at given baud rate
 }
 
 void loop() {
-	anVal = analogRead(readPin);  //read the input pin
-	
-  if (anVal > threshhold) {
-    Serial.println("Light");
-  } else {
-    Serial.println("Dark");
-  }
+  sens1 = analogRead(0);
+  sens2 = analogRead(1);
+  sens3 = analogRead(2);
+
+  Serial.print(sens1);
+  Serial.print(" - ");
+  Serial.print(sens2);
+  Serial.print(" - ");
+  Serial.print(sens3);
+  Serial.print("\n");
 
 }
