ff556a11acee7bc354911169b6f025565dabdce4
McQueenMotors <wspa4@lunet.lboro.ac.uk>
2023-12-11 14:50:03 +0000

    Update ButtonPressInput.ino
    The code now looks at the 2 smaller data arrays and puts them as 'LeftSide' and 'RightSide' of the barcode depending on the number they start with. If the format is incorrect then the prgram will re-scan

--- Barcode Scanner/ButtonPressInput/ButtonPressInput.ino
+++ Barcode Scanner/ButtonPressInput/ButtonPressInput.ino
@@ -2,71 +2,117 @@ const int buttonPin = 2;  // Change this to the pin where your button is connect
 const int ledPin = 13;
 const int arraySize = 67;
 bool BinCode[arraySize];
-bool SubArray1[28]; // Array from BinCode[3] to BinCode[30]
-bool SubArray2[28]; // Array from BinCode[36] to BinCode[63]
+bool SubArray1[28];
+bool SubArray2[28];  // Modified size to cover BinCode[36]-BinCode[63]
+bool LeftSide[28];
+bool RightSide[28];
+
+void scanBarcode() {
+  // Output the current value of the sensor every second
+  for (int i = 0; i < arraySize; i++) {
+    // Read and print the current sensor value
+    int sensorValue = digitalRead(buttonPin);
+    Serial.print("Current Sensor Status: ");
+    Serial.println(sensorValue);
+
+    // Add the button status to the array
+    BinCode[i] = !sensorValue; // Invert the value for active LOW
+    delay(1000);  // Wait for 1 second
+  }
+
+  // Check the first three digits of the array
+  if (!(BinCode[0] && !BinCode[1] && BinCode[2])) {
+    Serial.println("Invalid Barcode");
+    delay(2000);  // Wait for 2 seconds
+    // Clear the array
+    memset(BinCode, 0, sizeof(BinCode));
+    scanBarcode();  // Call the function recursively for a new scan
+  }
+
+  // Split BinCode into two subarrays
+  memcpy(SubArray1, BinCode + 3, 28 * sizeof(bool));
+  memcpy(SubArray2, BinCode + 36, 28 * sizeof(bool));  // Modified starting index
+
+  // Check and assign subarrays to LeftSide and RightSide
+  if (SubArray1[0] == 0 && SubArray2[0] == 1) {
+    memcpy(LeftSide, SubArray1, 28 * sizeof(bool));
+    memcpy(RightSide, SubArray2, 28 * sizeof(bool));
+  } else if (SubArray2[0] == 0 && SubArray1[0] == 1) {
+    memcpy(LeftSide, SubArray2, 28 * sizeof(bool));
+    memcpy(RightSide, SubArray1, 28 * sizeof(bool));
+  } else {
+    Serial.println("Invalid Subarrays");
+    // You may add additional logic or handling for invalid cases
+    memset(BinCode, 0, sizeof(BinCode));
+    scanBarcode();  // Call the function recursively for a new scan
+  }
+}
 
 void setup() {
+  //start serial connection
   Serial.begin(9600);
+  //configure pin 2 as an input and enable the internal pull-up resistor
+  pinMode(2, INPUT_PULLUP);
+  pinMode(13, OUTPUT);
   pinMode(buttonPin, INPUT_PULLUP); // Use INPUT_PULLUP for active LOW button
+  pinMode(ledPin, OUTPUT);
 
-  while (true) {
-    // Output the current value of the sensor every second
-    for (int i = 0; i < arraySize; i++) {
-      // Read and print the current sensor value
-      int sensorValue = digitalRead(buttonPin);
-      Serial.print("Current Sensor Status: ");
-      Serial.println(sensorValue);
+  // Perform the barcode scanning
+  scanBarcode();
 
-      // Add the button status to the array
-      BinCode[i] = !sensorValue; // Invert the value for active LOW
-      delay(1000);  // Wait for 1 second
-    }
-
-    // Print the array values to the Serial Monitor at the end
-    Serial.println("Binary Barcode: ");
-    for (int i = 0; i < arraySize; i++) {
-      Serial.print(BinCode[i]);
-      Serial.print(" ");
-    }
-    Serial.println();
-
-    // Check if the array starts with "101" and break the loop if true
-    if (BinCode[0] == 1 && BinCode[1] == 0 && BinCode[2] == 1) {
-      Serial.println("Binary Barcode starts with '101'.");
-      break;  // Exit the while loop
-    } else {
-      Serial.println("Binary Barcode does not start with '101'. Clearing the array and trying again.");
-      memset(BinCode, 0, sizeof(BinCode)); // Clear the array
-      delay(2000); // Wait for 2 seconds before retrying
-    }
+  // Print the array values to the Serial Monitor at the end
+  Serial.println("Binary Barcode: ");
+  for (int i = 0; i < arraySize; i++) {
+    Serial.print(BinCode[i]);
+    Serial.print(" ");
   }
+  Serial.println();
 
-  // Create SubArray1 from BinCode[3] to BinCode[30]
-  for (int i = 0; i < 28; i++) {
-    SubArray1[i] = BinCode[i + 3];
-  }
-
-  // Create SubArray2 from BinCode[36] to BinCode[63]
-  for (int i = 0; i < 28; i++) {
-    SubArray2[i] = BinCode[i + 36];
-  }
-
-  // Print the SubArrays to the Serial Monitor
-  Serial.println("SubArray1:");
+  // Print the subarrays
+  Serial.println("SubArray1: ");
   for (int i = 0; i < 28; i++) {
     Serial.print(SubArray1[i]);
     Serial.print(" ");
   }
   Serial.println();
 
-  Serial.println("SubArray2:");
+  Serial.println("SubArray2: ");
   for (int i = 0; i < 28; i++) {
     Serial.print(SubArray2[i]);
+    Serial.print(" ");
+  }
+  Serial.println();
+
+  // Check and assign subarrays to LeftSide and RightSide
+  if (SubArray1[0] == 0 && SubArray2[0] == 1) {
+    memcpy(LeftSide, SubArray1, 28 * sizeof(bool));
+    memcpy(RightSide, SubArray2, 28 * sizeof(bool));
+  } else if (SubArray2[0] == 0 && SubArray1[0] == 1) {
+    memcpy(LeftSide, SubArray2, 28 * sizeof(bool));
+    memcpy(RightSide, SubArray1, 28 * sizeof(bool));
+  } else {
+    Serial.println("Invalid Subarrays");
+    // You may add additional logic or handling for invalid cases
+    memset(BinCode, 0, sizeof(BinCode));
+    scanBarcode();  // Call the function recursively for a new scan
+  }
+
+  // Print the LeftSide and RightSide arrays
+  Serial.println("LeftSide: ");
+  for (int i = 0; i < 28; i++) {
+    Serial.print(LeftSide[i]);
+    Serial.print(" ");
+  }
+  Serial.println();
+
+  Serial.println("RightSide: ");
+  for (int i = 0; i < 28; i++) {
+    Serial.print(RightSide[i]);
     Serial.print(" ");
   }
   Serial.println();
 }
 
 void loop() {
-  // The loop function can be left empty for this example
+  // No need for loop logic since scanning is performed in the setup
 }
