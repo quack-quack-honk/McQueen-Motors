61df2808e7fe8418a16a3675bb023ddfc67b03be
Ben <doddsbt@gmail.com>
2024-04-05 16:31:07 +0100

    begin rewrite

---
+++ Line Detection Code/FullFollow/FullFollow.ino/FullFollow.ino.ino
@@ -0,0 +1,12 @@
+void setup() {
+  servoLeft.attach(13);
+  servoRight.attach(12);
+	Serial.begin(9600);   //setup serial at given baud rate
+  // servoLeft.writeMicroseconds(1570);  // Left wheel counterclockwise
+  // servoRight.writeMicroseconds(1430); // Right wheel counterclockwise
+}
+
+void loop() {
+  
+
+}
--- Line Detection Code/LineFollow/LineFollow.ino
+++ Line Detection Code/LineFollow/LineFollow.ino
@@ -9,8 +9,8 @@ void setup() {
   servoLeft.attach(13);
   servoRight.attach(12);
 	Serial.begin(9600);   //setup serial at given baud rate
-  servoLeft.writeMicroseconds(1570);  // Left wheel counterclockwise
-  servoRight.writeMicroseconds(1430); // Right wheel counterclockwise
+  // servoLeft.writeMicroseconds(1570);  // Left wheel counterclockwise
+  // servoRight.writeMicroseconds(1430); // Right wheel counterclockwise
 }
 
 void loop() {
