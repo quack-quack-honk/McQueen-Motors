f46a2c950a5df6b49eab1529f9850aa71b6c9c65
Ben <doddsbt@gmail.com>
2024-04-18 14:37:33 +0100

    added servo init

--- Line Detection Code/FullFollow/FullFollow.ino
+++ Line Detection Code/FullFollow/FullFollow.ino
@@ -1,9 +1,16 @@
+#include <Servo.h>                    // Include servo library
+ 
+Servo servoLeft;                      // Declare left and right servos
+Servo servoRight;
+bool left, centre, right;
+bool farLeft, farRight;
+
 void setup() {
+  
   servoLeft.attach(13);
   servoRight.attach(12);
 	Serial.begin(9600);   //setup serial at given baud rate
-  bool left, centre, right;
-  bool farLeft, farRight;
+
 
 
   int zone = 1;
