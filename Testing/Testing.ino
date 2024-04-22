a89b679d78f9c46115b41cb54e596eb8d50d84fe
Ben <doddsbt@gmail.com>
2024-04-22 18:42:43 +0100

    changes to testing code, and added all line following code

--- Testing/Testing.ino
+++ Testing/Testing.ino
@@ -8,7 +8,7 @@ bool farLeft, farRight;
 // servo values:
 // 1500 = stop
 // 1700 = max counter clockwise
-// 1300 = max clockwise
+// 1300 = max clockwise 
 
 void setup() {
   
@@ -16,6 +16,12 @@ void setup() {
   servoRight.attach(12);
 	Serial.begin(9600);   //setup serial at given baud rate
 
+  delay(250);
+  servoLeft.writeMicroseconds(1555); //max speed with calibration
+  servoRight.writeMicroseconds(1430); //max spee d with calibration
+//  delay(2000);
+//  servoLeft.writeMicroseconds(1490);
+//  servoRight.writeMicroseconds(1500);
 }
 
 void loop() {
