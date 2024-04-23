722bc6b1a5566310c1fb05c68d18002a98397d34
McQueenMotors <150973272+McQueenMotors@users.noreply.github.com>
2024-04-23 10:53:43 +0100

    Update StartingBarcode.ino
    Corrected errors in logic

--- Barcode Scanner/StartingBarcode/StartingBarcode.ino
+++ Barcode Scanner/StartingBarcode/StartingBarcode.ino
@@ -3,7 +3,7 @@ const int sensorPin2 = 2;
 const int sensorPin3 = 3;
 const int sensorPin4 = 4;
 const int sensorPin5 = 5;
-const int threshold = 500;
+const int threshold = 800;
 #include <Servo.h>
 Servo servoLeft;
 Servo servoRight;
@@ -20,56 +20,57 @@ void setup() {
   alignRobot();
 }
 
-void alignRobot() {
   int sensorValue1 = digitalRead(sensorPin1);
   int sensorValue5 = digitalRead(sensorPin5);
+
+void alignRobot() {
   while (true) {
     Serial.print("Sensor Value1: ");
     Serial.println(sensorValue1);
-    Serial.print("Sensor Value4: ");
-    Serial.println(sensorValue4);
+    Serial.print("Sensor Value5: ");
+    Serial.println(sensorValue5);
     
-    while(sensorValue1 == LOW && sensorValue5 == LOW){
-      moveForward()
+    while(sensorValue1 == HIGH && sensorValue5 == HIGH){
+      moveForward();
     }
 
-    if (sensorValue1 == HIGH && sensorValue5 == HIGH) {
+    if (sensorValue1 == LOW && sensorValue5 == LOW) {
       stopMotors();
       break;
     } else if (sensorValue1){
-        leftPivot()
+        leftPivot();
         // create code so that robot pivots around the sensor
     } else if (sensorValue5){
-        rightPivot()
+        rightPivot();
         // create code so that robot pivots around the sensor
     }
   }
 }
 
 void leftPivot() {
-  while (sensorValue1 == HIGH && sensorValue5 == LOW) {
-  // modify wheel speeds so robot pivots around sensor
-  }
-  moveBackward()
-  delay(1000)
-  alignRobot()
-}
-
-void rightPivot() {
   while (sensorValue1 == LOW && sensorValue5 == HIGH) {
   // modify wheel speeds so robot pivots around sensor
   }
-  moveBackward()
-  delay(1000)
-  alignRobot()
+  moveBackward();
+  delay(1000);
+  alignRobot();
+}
+
+void rightPivot() {
+  while (sensorValue1 == HIGH && sensorValue5 == LOW) {
+  // modify wheel speeds so robot pivots around sensor
+  }
+  moveBackward();
+  delay(1000);
+  alignRobot();
 }
 
 
 
 
 void moveForward() {
-  servoLeft.writeMicroseconds(1700);
-  servoRight.writeMicroseconds(1300);
+  servoLeft.writeMicroseconds(1520);
+  servoRight.writeMicroseconds(1480);
 }
 
 void turnLeft() {
